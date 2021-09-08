0b1a900b2bf401e533796155c15f8a4c7906b2a9
Ali Razzaq <alirazzaquet@gmail.com>
2021-09-08 20:08:27 +0500

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react"
+import Web3 from "web3";
 
 function App() {
+
+  const [data, setData] = useState({userAddress: ""});
+
+  window.ethereum.on('accountsChanged', function (accounts: string[]) {
+    setData(pre => { return { ...pre, userAddress: accounts[0] } })
+  })
+
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+      console.log(window.web3.currentProvider.isMetaMask)
+
+      // Get current logged in user address
+      const accounts = await window.web3.eth.getAccounts()
+      setData(pre => { return { ...pre, userAddress: accounts[0] } })
+      console.log(accounts[0])
+
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  };
+
+  useEffect(() => {
+    loadWeb3()
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Let's DeFi </h2>
+      <br />
+      {
+        data.userAddress ?
+          <div>You are login with Address: {data.userAddress}</div> :
+          <>
+            <div>Please Signin to Metamask</div>
+            <br />
+            <button onClick={() => loadWeb3()}> Connect </button>
+          </>
+      }
+
+      <br />
+
     </div>
   );
 }
